70b0028efb2ff7fdd9f18e968bec1f51c00bdfd2
Peter Lotts <pml43@cam.ac.uk>
2018-01-13 21:03:53 +0000

    Modify benchmark script to put the test system under load

--- ipc-bench.sh
+++ ipc-bench.sh
@@ -13,7 +13,9 @@ echo "Measured transfer rate" > $csv
 while ($n < 100)
 	# Allow the system to calm down before running benchmark
 	sleep 2
-	$ipc -i tcp 2proc | awk '{print $1}' >> $csv
+	# 16GiB transfer is found to put the system under significant
+	# load for about 10 seconds, on my machine
+	$ipc -i tcp -t 17179869184 2proc | awk '{print $1}' >> $csv
 	@ n += 1
 end
 
